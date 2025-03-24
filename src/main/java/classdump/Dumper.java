6485b7c5d213f71e34b20444738dcb2d43c1b975
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2025-03-25 06:41:33 +0800

    screw the two jars i made them one

--- src/main/java/classdump/Dumper.java
+++ src/main/java/classdump/Dumper.java
@@ -1,38 +1,20 @@
 package classdump;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.ProtectionDomain;
-import java.util.Comparator;
-import java.util.stream.Stream;
 
 @SuppressWarnings("rawtypes")
 public final class Dumper implements ClassFileTransformer {
-    private static final Path DIR = Paths.get("_classdump");
+    private static final File DIR;
 
     static {
-        if (Files.exists(DIR))
-            try {
-                try (Stream<Path> walk = Files.walk(DIR)) {
-                    walk.sorted(Comparator.reverseOrder()).forEach(Dumper::rm);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-    }
-
-    private static void rm(Path path) {
-        try {
-            Files.delete(path);
-        } catch (IOException e) {
-            throw new RuntimeException(path.toString(), e);
-        }
+        File dir = DIR = new File("_classdump");
+        if (dir.exists())
+            rm(dir);
     }
 
     private final boolean debug;
@@ -41,56 +23,76 @@ public final class Dumper implements ClassFileTransformer {
         this.debug = debug;
     }
 
+    private static void rm(File file) {
+        if (file.isDirectory())
+            //noinspection DataFlowIssue
+            for (File f : file.listFiles())
+                rm(f);
+        if (!file.delete())
+            throw new RuntimeException("Failed to delete ".concat(file.getPath()));
+    }
+
     public static void premain(String args, Instrumentation inst) {
         boolean debug = "debug".equalsIgnoreCase(args);
         inst.addTransformer(new Dumper(debug));
-        Class[] classes = inst.getAllLoadedClasses();
 
         if (debug)
-            System.out.println("Dumping classes to " + DIR.toAbsolutePath());
+            System.out.println("Dumping classes to ".concat(DIR.getAbsolutePath()));
 
         //already loaded classes
-        for (Class cls : classes) {
+        for (Class cls : inst.getAllLoadedClasses()) {
             if (cls.isArray())
                 continue;
             String name = cls.getName().replace('.', '/');
             try {
-                String clsName = name + ".class";
-                InputStream in = cls.getResourceAsStream("/" + clsName);
+                String clsName = name.concat(".class");
+                InputStream in = cls.getResourceAsStream("/".concat(clsName));
                 if (in != null) {
                     try {
-                        Path path = DIR.resolve(clsName);
-                        Files.createDirectories(path.getParent());
-                        Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
+                        File file = new File(DIR, clsName);
+                        //noinspection ResultOfMethodCallIgnored ???????? (what)
+                        file.getParentFile().mkdirs();
+                        FileOutputStream out = new FileOutputStream(file);
+                        try {
+                            byte[] buf = new byte[8 * 1024];
+                            for (int i; (i = in.read(buf)) != -1;)
+                                out.write(buf, 0, i);
+                        } finally {
+                            out.close();
+                        }
                     } finally {
                         in.close();
                     }
                     if (debug)
-                        System.out.println("Dumped class " + name);
+                        System.out.println("Dumped class ".concat(name));
                 } else if (debug)
-                    System.out.println("Unable to get bytecode for " + name);
+                    System.out.println("Unable to get bytecode for ".concat(name));
             } catch (Exception e) {
-                System.err.println("Unable to dump class " + name);
+                System.err.println("Unable to dump class ".concat(name));
                 //noinspection CallToPrintStackTrace
                 e.printStackTrace();
             }
         }
 
-
         if (debug)
-            System.out.println(inst.getAllLoadedClasses().length + " classes loaded during initialization");
+            System.out.println(String.valueOf(inst.getAllLoadedClasses().length).concat(" classes loaded during initialization"));
     }
 
-    @Override
     public byte[] transform(ClassLoader classLoader, String name, Class clazz, ProtectionDomain protectionDomain, byte[] bytes) {
         try {
-            Path path = DIR.resolve(name + ".class");
-            Files.createDirectories(path.getParent());
-            Files.write(path, bytes);
+            File file = new File(DIR, name.concat(".class"));
+            //noinspection ResultOfMethodCallIgnored ???????? (what)
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            try {
+                out.write(bytes);
+            } finally {
+                out.close();
+            }
             if (debug)
-                System.out.println("Dumped class " + name);
+                System.out.println("Dumped class ".concat(name));
         } catch (Exception e) {
-            System.err.println("Unable to dump class " + name);
+            System.err.println("Unable to dump class ".concat(name));
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
